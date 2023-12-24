087a4dfb814f6644f4e676c9868d670ede3dac68
lzc <3030218083@qq.con>
2023-12-24 19:11:09 +0800

    update

--- src/include/data_structure/hashtable.h
+++ src/include/data_structure/hashtable.h
@@ -1,7 +1,38 @@
-/*
-hashtbale.h
-hash
+/**
+ * @file hashtable.h
+ * @author lzc (liuzechen.coder@qq.com)
+ * @brief hashtbale的头文件
+ * @version 0.1
+ * @date 2023-12-24
+ *
+ * @copyright Copyright (c) 2023
+ */
 
+#ifndef REDIS_PP_HASHTABLE_H
+#define REDIS_PP_HASHTABLE_H
 
+namespace my_redis {
+/**
+ * @brief HashTable类
+ *
+ */
+class HashTable {
+public:
+    /**
+     * @brief HashTable上的节点
+     *
+     */
+    class HNode {
+    public:
+    private:
+    };
 
-*/
+public:
+private:
+    HNode** m_table { nullptr };
+    
+    
+};
+} // namespace my_redis
+
+#endif
